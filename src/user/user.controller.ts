554226733362b2342681ea23613de77ec57624f9
Alexey <algoritm211@gmail.com>
2021-11-04 14:00:00 +0200

    Add routes for getting user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,28 @@
-import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  HttpCode,
+  HttpStatus,
+  Get,
+  Param,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async getAll() {
+    return this.userService.getAll();
+  }
+
+  @Get(':id')
+  async getOne(@Param('id') id: string) {
+    return this.userService.getOne(id);
+  }
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { User } from './user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 
@@ -7,6 +7,21 @@ export class UserService {
   constructor(
     @Inject('USER_REPOSITORY') private readonly userRepository: typeof User,
   ) {}
+
+  async getAll() {
+    return this.userRepository.findAll();
+  }
+
+  async getOne(id: string) {
+    const user = await this.userRepository.findByPk<User>(id);
+    if (!user) {
+      throw new HttpException(
+        'User with given id not found',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    return user;
+  }
 
   async create(userData: CreateUserDto): Promise<User> {
     return this.userRepository.create(userData, { returning: true });
